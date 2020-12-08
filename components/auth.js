6157122cd1154c602f2f32734ca2886e411f3aa0
whatadeveloper <75500162+whatadeveloper@users.noreply.github.com>
2020-12-08 12:18:15 +0530

    Update auth.js

--- components/auth.js
+++ components/auth.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 const fs = require('fs');
 
 router.get('/checkauth', async (req, res) => {
-    client.getState().then((data) => {
-        console.log(data)
-        res.send(data)
-    }).catch((err) => {
-        if(err){
-            res.send("DISCONNECTED")
-            try{
-                fs.unlinkSync('../../session.json')
-            }catch(err){console.log(err)}
-        }
-    })
+//     client.getState().then((data) => {
+//         console.log(data)
+//         res.send(data)
+//     }).catch((err) => {
+//         if(err){
+//             res.send("DISCONNECTED")
+//             try{
+//                 fs.unlinkSync('../../session.json')
+//             }catch(err){console.log(err)}
+//         }
+//     })
 });
 
 router.get('/logout', async () => {
